e1f4cf281fc384d831119875a991645630e1a42a
Nik Silver <nik@niksilver.com>
2013-01-25 12:59:10 +0000

    Can place (any number of) items on the queue.

--- src/main/scala/com/bscl/InstructionQueue.scala
+++ src/main/scala/com/bscl/InstructionQueue.scala
@@ -3,6 +3,18 @@ package com.bscl
 /**
  * Basic implementation of the BSCL instruction queue.
  */
-class InstructionQueue {
-
+class InstructionQueue private(val messages: List[InstructionMessage]) {
+  /**
+   * Construct and empty queue
+   */
+  def this() = this(List())
+  
+  def place(m: InstructionMessage) =
+    new InstructionQueue(messages ++ List(m))
 }
+
+
+/**
+ * Simple implementation of an instruction message.
+ */
+case class InstructionMessage(instructionType: Int)
--- src/test/scala/com/bscl/InstructionQueueSuite.scala
+++ src/test/scala/com/bscl/InstructionQueueSuite.scala
@@ -7,4 +7,15 @@ class InstructionQueueSuite extends FunSuite with ShouldMatchers {
   test("Can construct queue") {
     val queue = new InstructionQueue
   }
+  
+  test("Supports any number of instruction messages") {
+    val m0 = InstructionMessage(0)
+    val m1 = InstructionMessage(1)
+    val m2 = InstructionMessage(2)
+    
+    val queue = new InstructionQueue
+    val queue2 = queue.place(m0).place(m1).place(m2)
+    
+    queue2.messages should equal (List(m0, m1, m2))
+  }
 }
